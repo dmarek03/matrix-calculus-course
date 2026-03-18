84889a2e2430103cd0a9fb84de684fe767b7b8a2
dmarek03 <domarek@student.agh.edu.pl>
2026-03-18 20:35:05 +0100

    feat(plots): support multiple curves on a single plot

--- report_1/utils/plots.py
+++ report_1/utils/plots.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
 
 
-def plot_results(sizes, times, ops_counts, title_prefix=""):
+def plot_results_multi(sizes, results, title_prefix=""):
+    fig, ax = plt.subplots(1, 2, figsize=(14, 6))
 
-    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+    for label, (times, ops_counts) in results.items():
+        ax[0].plot(sizes, times, marker="o", label=label)
+        ax[1].plot(sizes, ops_counts, marker="o", label=label)
 
-    ax[0].plot(sizes, times, marker='o')
     ax[0].set_xlabel("Rozmiar macierzy (n)")
     ax[0].set_ylabel("Czas wykonania [s]")
     ax[0].set_title(f"{title_prefix} Czas wykonania vs rozmiar macierzy")
+    ax[0].set_yscale("log")
     ax[0].grid(True)
+    ax[0].legend()
 
-    ax[1].plot(sizes, ops_counts, marker='o', color='red')
     ax[1].set_xlabel("Rozmiar macierzy (n)")
     ax[1].set_ylabel("Liczba operacji zmiennoprzecinkowych")
     ax[1].set_title(f"{title_prefix} Liczba operacji vs rozmiar macierzy")
     ax[1].grid(True)
+    ax[1].legend()
 
     plt.tight_layout()
     plt.show()
-
