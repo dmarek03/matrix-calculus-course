b2fa12ff0a3e2ca94cb715eb1da41c5b1ec6f815
dmarek03 <domarek@student.agh.edu.pl>
2026-03-27 20:06:39 +0100

    fix: hybrid algorithms switching

--- report_1/algorithms/hybrid.py
+++ report_1/algorithms/hybrid.py
@@ -2,13 +2,12 @@ from .binet import binet_multiply
 from .classical import classical_multiply
 
 
-def hybrid_multiply(matrix_1, matrix_2, l_size=2):
+def hybrid_multiply(matrix_1, matrix_2, l_size=32):
     n = len(matrix_1)
 
     if n & (n - 1) != 0:
         raise ValueError("Matrix size must be a power of 2.")
 
-    if n <= 2**l_size:
+    if n <= l_size:
         return classical_multiply(matrix_1, matrix_2)
-
     return binet_multiply(matrix_1, matrix_2)
