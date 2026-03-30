17babf8b045655d327f4d17e54107111768bea90
dmarek03 <domarek@student.agh.edu.pl>
2026-03-31 00:48:10 +0200

    fix(lu_with_pivot): adjust L during row swap

--- report_2/algortihms/lu_factorization_with_pivot.py
+++ report_2/algortihms/lu_factorization_with_pivot.py
@@ -10,11 +10,21 @@ def lu_with_pivot(A):
 
     for k in range(n):
         pivot_index = np.argmax(np.abs(A[k:, k])) + k
+
         if pivot_index != k:
             A[[k, pivot_index], :] = A[[pivot_index, k], :]
             P[[k, pivot_index]] = P[[pivot_index, k]]
+
+            if k > 0:
+                L[[k, pivot_index], :k] = L[[pivot_index, k], :k]
+
         U[k, k:] = A[k, k:]
-        for i in range(k+1, n):
+
+        if abs(U[k, k]) < 1e-12:
+            raise ValueError("Macierz osobliwa")
+
+        for i in range(k + 1, n):
             L[i, k] = A[i, k] / U[k, k]
-            A[i, k+1:] -= L[i, k] * U[k, k+1:]
+            A[i, k + 1 :] -= L[i, k] * U[k, k + 1 :]
+
     return L, U, P
