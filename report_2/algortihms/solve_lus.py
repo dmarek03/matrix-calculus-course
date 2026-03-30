9a0b7767dd1ff3a76c3ef7f54f909a04ed752bac
dmarek03 <domarek@student.agh.edu.pl>
2026-03-31 00:53:03 +0200

    refactor: clean up solve_lu with pivoting support

--- report_2/algortihms/solve_lus.py
+++ report_2/algortihms/solve_lus.py
@@ -1,24 +1,28 @@
-from algortihms.lu_factorization_with_pivot import lu_with_pivot
-from algortihms.lu_factorization import lu_decomposition
+from .lu_factorization_with_pivot import lu_with_pivot
+from .lu_factorization import lu_decomposition
 import numpy as np
 
 
 def solve_lu(A, b, algorithm):
-    if algorithm == 'lu_with_pivot':
+    if algorithm == "lu_with_pivot":
         L, U, P = lu_with_pivot(A)
-        b_perm = b[P]  # permutacja potrzebna tylko przy pivotingu
-    else:        
-        L, U, P = lu_decomposition(A)  # czy lu_decomposition przyjmuje tylko A?
-        b_perm = b[P] if P is not None else b.copy()
-    
+    else:
+        L, U, P = lu_decomposition(A)
+
+    b_perm = b[P]
+
     n = len(b)
 
+    # forward substitution (Ly = Pb)
     y = np.zeros(n)
     for i in range(n):
         y[i] = b_perm[i] - np.dot(L[i, :i], y[:i])
-    
+
+    # backward substitution (Ux = y)
     x = np.zeros(n)
-    for i in range(n-1, -1, -1):
-        x[i] = (y[i] - np.dot(U[i, i+1:], x[i+1:])) / U[i, i]
-    
+    for i in range(n - 1, -1, -1):
+        if abs(U[i, i]) < 1e-12:
+            raise ValueError("Dzielenie przez zero w U")
+        x[i] = (y[i] - np.dot(U[i, i + 1:], x[i + 1:])) / U[i, i]
+
     return x
